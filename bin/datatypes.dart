10a1337f6fe6130c22237c1f947452d4625591a7
muhammedfasalc <muhammedfasal32@gmail.com>
2023-09-27 18:14:55 +0530

    first commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,20 @@
+void main(){
+String name ='anu';
+int age     =21;
+int phone   =9061188464;
+double mark =8.2;
+String email='muhammedfasal32@gmail.com';
+
+
+print('my name is $name');
+print(' Iam $age yrs old');
+print('my phone number is $phone');
+print('$cgpa is my CGPA in graduation');
+print('my email id is$email');
+
+
+
+
+
+
+}
---
+++ bin/first_sample.dart
@@ -0,0 +1,6 @@
+void main(){
+print("fasal");
+print(21);
+print(9061188464);
+print(8.2);
+}
