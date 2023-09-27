0709132a7ae2702a7eef672b1692886b51685dbf
muhammedfasalc <muhammedfasal32@gmail.com>
2023-09-27 18:23:36 +0530

    commit

--- bin/datatypes.dart
+++ bin/datatypes.dart
@@ -1,20 +1,13 @@
-void main(){
-String name ='anu';
-int age     =21;
-int phone   =9061188464;
-double mark =8.2;
-String email='muhammedfasal32@gmail.com';
+void main() {
+  String name = 'anu';
+  int age = 21;
+  int phone = 9061188464;
+  double mark = 8.2;
+  String email = 'muhammedfasal32@gmail.com';
 
-
-print('my name is $name');
-print(' Iam $age yrs old');
-print('my phone number is $phone');
-print('$cgpa is my CGPA in graduation');
-print('my email id is$email');
-
-
-
-
-
-
+  print('my name is $name');
+  print(' Iam $age yrs old');
+  print('my phone number is $phone');
+  print('$cgpa is my CGPA in graduation');
+  print('my email id is$email');
 }
